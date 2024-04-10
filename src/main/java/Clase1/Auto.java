27a2a7ff720511482354ed574dd4174637573c57
Ricardo Leiton <ricardo.leiton@gmail.com>
2024-04-10 18:36:59 -0300

    Programacion 2 - Clase 2

--- src/main/java/Clase1/Auto.java
+++ src/main/java/Clase1/Auto.java
@@ -7,10 +7,18 @@ public class Auto {
     private int anio;
     private boolean revisionTecnica;
 
+    //Clase constructora
     public Auto(String marca, String modelo, int anio, boolean revisionTecnica) {
         this.marca = marca;
         this.modelo = modelo;
         this.anio = anio;
         this.revisionTecnica = revisionTecnica;
     }
+
+    @Override
+    public String toString() {
+        return "Auto{" + "marca=" + marca + ", modelo=" + modelo + ", anio=" + anio + ", revisionTecnica=" + revisionTecnica + '}';
+    }
+    
+    
 }
